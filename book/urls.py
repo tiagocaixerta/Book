e2348acceda1f57925efc14378dd389e838b30ac
Tiago Caixerta <caixerta63@gmail.com>
2025-06-13 10:13:46 -0300

    ajuste

--- book/urls.py
+++ book/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# Arquivo de rotas da aplicação de produtos
 
 from django.contrib import admin
 from django.urls import path

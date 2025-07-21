bad5b5f433cf39cf411ab27dc93782efb921a9c1
Tiago Caixerta <caixerta63@gmail.com>
2025-07-21 14:58:07 -0300

    Permitir acesso público à listagem de produtos

--- product/viewsets/product_viewset.py
+++ product/viewsets/product_viewset.py
@@ -1,10 +1,11 @@
 from rest_framework.viewsets import ModelViewSet
-
+from rest_framework.permissions import AllowAny  
 from product.models import Product
 from product.serializers.product_serializer import ProductSerializer
 
 class ProductViewSet(ModelViewSet):
     serializer_class = ProductSerializer
+    permission_classes = [AllowAny]  
 
     def get_queryset(self):
         return Product.objects.all()
